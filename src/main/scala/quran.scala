e98e507cbafe36798545c325969d605a631dd88c
Milad Khajavi <khajavi@gmail.com>
2017-03-16 22:00:44 +0330

    add surah and ayah number

--- src/main/scala/quran.scala
+++ src/main/scala/quran.scala
@@ -1,8 +1,6 @@
-import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import akka.stream.ActorMaterializer
@@ -12,7 +10,6 @@ import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.io.StdIn
 import scala.xml._
 
 class Aya(source: NodeSeq) {
@@ -46,69 +43,27 @@ object Quran {
 
   def main(args: Array[String]) {
     val quran = Quran.fromFile("quran-simple-enhanced.xml")
-    println(quran.suras(1))
-    println(quran.suras(2).ayas(3).text)
 
-    // domain model
-    final case class Item(name: String, id: Long)
-    final case class Order(items: List[Item])
-    case class Aya(number: Int, text: String)
+    case class Aya(surah: Int, ayah: Int, text: String)
 
-
-    // (fake) async database query api
-    def fetchItem(itemId: Long): Future[Option[Item]] = Future {
-      Some(Item("a", 1L))
-    }
-    def saveOrder(order: Order): Future[Done] = Future {
-      Done
-    }
-
-
-    // needed to run the route
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
 
-    // formats for unmarshalling and marshalling
-    implicit val itemFormat = jsonFormat2(Item)
-    implicit val orderFormat = jsonFormat1(Order)
-    implicit val ayaFormat = jsonFormat2(Aya)
-
+    implicit val ayaFormat = jsonFormat3(Aya)
 
     val settings = CorsSettings.defaultSettings.copy(allowCredentials = false)
 
-
     val route: Route =
       handleRejections(CorsDirectives.corsRejectionHandler) {
-
         CorsDirectives.cors(settings) {
           handleRejections(RejectionHandler.default) {
-            get {
-              pathPrefix("item" / LongNumber) { id =>
-                // there might be no item for a given id
-                val maybeItem: Future[Option[Item]] = fetchItem(id)
-
-                onSuccess(maybeItem) {
-                  case Some(item) => complete(item)
-                  case None => complete(StatusCodes.NotFound)
+            path(IntNumber / IntNumber) {
+              case (surah: Int, ayah: Int) => complete(
+                Future {
+                  Aya(surah, ayah, quran.suras(surah).ayas(ayah).text)
                 }
-              } ~ path(IntNumber / IntNumber) {
-                case (a: Int, b) => complete(
-                  Future {
-                    Aya(a, quran.suras(a).ayas(b).text)
-                  }
-                )
-              }
-            } ~
-              post {
-                path("create-order") {
-                  entity(as[Order]) { order =>
-                    val saved: Future[Done] = saveOrder(order)
-                    onComplete(saved) { done =>
-                      complete("order created")
-                    }
-                  }
-                }
-              }
+              )
+            }
           }
         }
       }
@@ -118,7 +73,7 @@ object Quran {
     val host = conf.getString("http.host")
 
     val bindingFuture = Http().bindAndHandle(RouteResult.route2HandlerFlow(route), host, port)
-    println(s"Server online at http://$host:$port/\nPress RETURN to stop...")
+    println(s"Server online at http://$host:$port")
   }
 }
 
